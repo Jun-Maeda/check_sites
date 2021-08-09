234f8006ad141564af79857b9c039c062b22caf4
maeda <jun_pure@icloud.com>
2021-08-09 20:55:15 +0900

    test4

--- main.py
+++ main.py
@@ -5,13 +5,9 @@ from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from check_site import cosme_bs, kao_bs, m_donguri_bs, m_donguri_bs, k_donguri_bs, send_message
 import os
-import sys
 
 
 if __name__ == "__main__":
-    args = sys.argv
-    # access_token = os.environ.get('ACCESS_TOKEN')
-    access_token = args[1]
-    user_id = args[2]
-    # user_id = os.environ.get('LINE_ID')
+    access_token = os.environ.get('ACCESS_TOKEN').encode('utf-8')
+    user_id = os.environ.get('LINE_ID').encode('utf-8')
     send_message(access_token, user_id, "test")
