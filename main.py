b30783bd94fabf745a44c91f851c444b88b8471a
maeda <jun_pure@icloud.com>
2021-08-21 09:54:37 +0900

    test

--- main.py
+++ main.py
@@ -10,5 +10,4 @@ import os
 if __name__ == "__main__":
     access_token = os.environ.get('ACCESS_TOKEN ')
     user_id = os.environ.get('LINE_ID ')
-    # LINEを送る
     send_line(access_token, user_id, "test")
